ca62b1e41a3a1966266a9184581ac77614dd8b7f
cmdr <cmdr@commander23.net>
2025-07-27 23:54:13 +0000

    getting ready to monitor whole of line 11

--- python/main.py
+++ python/main.py
@@ -2,45 +2,61 @@ from vvo_api import vvo_api_pointfinder, vvo_api_departure_monitor, vvo_api_trip
 from static_vvo import write_to_json, web_get_json, load_geojson, search_geojson
 from datetime import datetime, timedelta
 
-userinput = input("Enter a stop name or ID: ")
-stop_id = vvo_api_pointfinder(userinput, limit=10, stopsOnly=True)[0]
+
+"""
+stopid = 0
 lines = vvo_api_lines(stop_id)
-
-mot = ["Tram", "CityBus", "IntercityBus", "SuburbanRailway", "Train", "Cableway", "Ferry", "HailedSharedTaxi"]
-
 pointfinder = vvo_api_pointfinder(stop_id, limit=10, stopsOnly=True, regionalOnly=False, stopShortcuts=False)
-departures = vvo_api_departure_monitor(stop_id, limit=10, time='', isarrival=False, shorttermchanges=False, mot = mot)
 trip_details =None #vvo_api_trip_details(tripid='', time='', stop_id = stop_id, mapdata=False)
 query_trip = None #vvo_api_query_trip(origin='', destination='', shorttermchanges=False, time='', isArrivalTime=False)
 lines = vvo_api_lines(stop_id)
-
-print("\n"*3)
-print(f"Station Name: {departures['Name']}")
-print(f"City: {departures['Place']}")
-
-Expiery_Time = vvo_timestamp_to_datetime_class(departures['ExpirationTime'])[0]
-TimeZone = vvo_timestamp_to_datetime_class(departures['ExpirationTime'])[1]
-print(f"Expiery Time: {Expiery_Time + TimeZone}")
-
-# Print the departure times
-print("\nDeparture Times:")
-for departure in departures['Departures']:
-    print(f"- {departure['LineName']} to {departure['Direction']}")
-    if 'RealTime' in departure:
-        print(f"   Real-time Departure: {vvo_timestamp_to_datetime_class(departure['RealTime'])[0] + TimeZone}")
-    print(f"   Scheduled Departure: {vvo_timestamp_to_datetime_class(departure['ScheduledTime'])[0] + TimeZone}")
-    print(f"   Platform: {departure['Platform']['Name']}")
-
-
 """
-write_to_json(pointfinder, "/home/cmdr/tramrunner/files/api_response_pointfinder.json")
+"""
 write_to_json(departures, "/home/cmdr/tramrunner/files/api_response_departures.json")
+write_to_json(pointfinder, "/home/cmdr/tramrunner/files/api_response_pointfinder.json")
 write_to_json(trip_details, "/home/cmdr/tramrunner/files/api_response_trip_details.json")
 write_to_json(query_trip, "/home/cmdr/tramrunner/files/api_response_query_trip.json")
 write_to_json(lines, "/home/cmdr/tramrunner/files/api_response_lines.json")
 """
 
+def line_info_tui():
+    userinput = "rac"#input("Enter a stop name or ID: ")
+    stop_id = vvo_api_pointfinder(userinput, limit=10, stopsOnly=True)[0]
+    mot = ["Tram", "CityBus", "IntercityBus", "SuburbanRailway", "Train", "Cableway", "Ferry", "HailedSharedTaxi"]
+    departures = vvo_api_departure_monitor(stop_id, limit=10, time='', isarrival=False, shorttermchanges=False, mot = mot)
+    print(departures)
 
-#print(stop_id)
-#print(lines)
-#print(departure)['Departures']
+    query_trip = vvo_api_query_trip("ztz", "bue", shorttermchanges=False, time='', isArrivalTime=False)
+
+    write_to_json(query_trip, "/home/cmdr/tramrunner/files/api_response_query_trip.json")
+    write_to_json(departures, "/home/cmdr/tramrunner/files/api_response_departures.json")
+    
+def departure_monitor_tui():
+    userinput = input("Enter a stop name or ID: ")
+    stop_id = vvo_api_pointfinder(userinput, limit=10, stopsOnly=True)[0]
+    mot = ["Tram", "CityBus", "IntercityBus", "SuburbanRailway", "Train", "Cableway", "Ferry", "HailedSharedTaxi"]
+    departures = vvo_api_departure_monitor(stop_id, limit=10, time='', isarrival=False, shorttermchanges=False, mot = mot)
+
+    print("\n"*3)
+    print(f"Station Name: {departures['Name']}")
+    print(f"City: {departures['Place']}")
+
+    Expiery_Time = vvo_timestamp_to_datetime_class(departures['ExpirationTime'])[0]
+    TimeZone = vvo_timestamp_to_datetime_class(departures['ExpirationTime'])[1]
+    print(f"Expiery Time: {Expiery_Time + TimeZone}")
+
+    # Print the departure times
+    print("\nDeparture Times:")
+    for departure in departures['Departures']:
+        print(f"- {departure['LineName']} to {departure['Direction']}")
+        if 'RealTime' in departure:
+            print(f"   Real-time Departure: {vvo_timestamp_to_datetime_class(departure['RealTime'])[0] + TimeZone}")
+        print(f"   Scheduled Departure: {vvo_timestamp_to_datetime_class(departure['ScheduledTime'])[0] + TimeZone}")
+        print(f"   Platform: {departure['Platform']['Name']}")
+
+
+
+
+
+#departure_monitor_tui()
+line_info_tui()
--- python/vvo_api.py
+++ python/vvo_api.py
@@ -2,24 +2,40 @@ import requests
 import json
 from datetime import datetime, timedelta
 
-headers = {
+default_headers = {
     "Content-Type": "application/json",
     "charset": "utf-8"
     }
 
+def query_vvo_api(url: str, headers: dict, params: dict = None) -> dict:
+    """
+    Query the VVO API to get information about stops, departures, and trips.
+    """
+    if not url:
+        raise ValueError("URL cannot be empty.")
+    
+    try:
+        response = requests.get(url, params=params, headers=headers, timeout=5, verify=True) 
+        if response.status_code == 200:
+            content = json.loads(response.content.decode('utf-8'))
+            
+            output = content #content['Points'][0].split('|')
+            # outputs the first point in the list, e.g. ['33000313', '', 'Räcknitzhöhe', '5655709', '4622355', '0', '']
+            
+            return output
+        else:
+            raise requests.HTTPError('HTTP Status: {}'.format(response.status_code))    
+    except requests.RequestException as e:
+        print(f"Failed to access VVO pointfinder. Request Exception", e)
+        response = None
+    
+    if response is None:
+        return None
 
-def vvo_api_pointfinder(query: str, limit: int = 0, stopsOnly: bool = False, regionalOnly: bool = False, stopShortcuts: bool = False) -> dict:
+
+def vvo_api_pointfinder(query: str, limit: int = 0, stopsOnly: bool = False, regionalOnly: bool = False, stopShortcuts: bool = False):
     """
     Find stops based certain parameters.
-    Arguments:
-        query, lim
-        query : Search query.
-        limit : The maximum number of results to return.
-        stopsOnly : If True, only return stops.
-        regionalOnly : If True, restrict results to regional stops.
-        stopShortcuts : If True, include stop shortcuts in the results.
-    Returns:
-        {'PointStatus': 'Identified', 'Status': {'Code': 'Ok'}, 'Points': ['33000313|||Räcknitzhöhe|5655709|4622355|0||'], 'ExpirationTime': '/Date(1753115786301+0200)/'}
     """
    
     defaulturl = "https://webapi.vvo-online.de/tr/pointfinder"
@@ -39,7 +55,7 @@ def vvo_api_pointfinder(query: str, limit: int = 0, stopsOnly: bool = False, reg
 
 
     try:
-        response = requests.get(defaulturl, params=params, headers=headers, timeout=5, verify=True) 
+        response = requests.get(defaulturl, params=params, headers=default_headers, timeout=5, verify=True) 
         if response.status_code == 200:
             content = json.loads(response.content.decode('utf-8'))
             # response {'PointStatus': 'Identified', 'Status': {'Code': 'Ok'}, 'Points': ['33000313|||Räcknitzhöhe|5655709|4622355|0||'], 'ExpirationTime': '/Date(1753115786301+0200)/'}
@@ -57,19 +73,38 @@ def vvo_api_pointfinder(query: str, limit: int = 0, stopsOnly: bool = False, reg
     if response is None:
         return None
 
-
-
-def vvo_api_departure_monitor(stopid: str, limit: int = 0, time: str = '' , isarrival: bool = False, shorttermchanges: bool = False, mot: list = None) -> None:
+def vvo_api_departure_monitor(stopid: str, limit: int = 0, time: str = '' , isarrival: bool = False, shorttermchanges: bool = False, mot: list = None):
     """
-    List out upcoming departures from a stop id.
-    Arguments:   
-        stopid : ID of the stop	
-        limit : Maximum number of results
-        time : ISO8601 timestamp, e.g. 2017-02-22T15:40:26Z
-        isarrival : Is the time specified above supposed to be interpreted as arrival or departure time?
-        shorttermchanges : unknown in this context
-        mot : Allowed modes of transport, see below
-            Currently accepted modes of transport are `Tram`, `CityBus`, `IntercityBus`, `SuburbanRailway`, `Train`, `Cableway`, `Ferry`, `HailedSharedTaxi`.    
+    Get the departures from a stop by stopid.
+    """
+    """
+    Response:
+        {
+            "Name": "Räcknitzhöhe",
+            "Status": {"Code": "Ok"},
+            "Place": "Dresden",
+            "ExpirationTime": "/Date(1753468523932+0200)/",
+            "Departures": [
+                {
+                    "Id": "voe:21085: :H:j25",
+                    "DlId": "de:vvo:21-85",
+                    "LineName": "85",
+                    "Direction": "Löbtau Süd",
+                    "Platform": {"Name": "2", "Type": "Platform"},
+                    "Mot": "CityBus",
+                    "RealTime": "/Date(1753468500000-0000)/",
+                    "ScheduledTime": "/Date(1753468560000-0000)/",
+                    "State": "InTime",
+                    "RouteChanges": ["23520", "23448"],
+                    "Diva": {"Number": "21085", "Network": "voe"},
+                    "CancelReasons": [],
+                    "Occupancy": "Unknown"
+                },
+                {
+                ...
+                }
+            ]
+        } 
     """
     
     defaulturl = "https://webapi.vvo-online.de/dm"
@@ -78,7 +113,7 @@ def vvo_api_departure_monitor(stopid: str, limit: int = 0, time: str = '' , isar
     if mot is None:
         mot = ["Tram", "CityBus", "IntercityBus", "SuburbanRailway", "Train", "Cableway", "Ferry", "HailedSharedTaxi"]
 
-    params = {
+    query_params = {
         "stopid": stopid,
         "limit": limit,
         "time": time,
@@ -87,30 +122,9 @@ def vvo_api_departure_monitor(stopid: str, limit: int = 0, time: str = '' , isar
         "mot": mot
     }
 
+    return query_vvo_api(defaulturl, default_headers, query_params)
 
-
-    try:
-        response = requests.get(defaulturl, params=params, headers=headers, timeout=5, verify=True) 
-        if response.status_code == 200:
-            content = json.loads(response.content.decode('utf-8'))
-            
-            output = content #content['Points'][0].split('|')
-            # outputs the first point in the list, e.g. ['33000313', '', 'Räcknitzhöhe', '5655709', '4622355', '0', '']
-            
-            return output
-        else:
-            raise requests.HTTPError('HTTP Status: {}'.format(response.status_code))    
-    except requests.RequestException as e:
-        print(f"Failed to access VVO pointfinder. Request Exception", e)
-        response = None
-    
-    if response is None:
-        return None
-
-
-
-
-def vvo_api_trip_details(tripid: str, time: str, stopid: str, mapdata: bool = False) -> None:
+def vvo_api_trip_details(tripid: str, time: str, stopid: str, mapdata: bool = False):
     """
     Get Details about the stations involved in a trip.
     Arguments:
@@ -124,16 +138,16 @@ def vvo_api_trip_details(tripid: str, time: str, stopid: str, mapdata: bool = Fa
     if not tripid or not time or not stopid:
         raise ValueError("Trip ID, time, and stop ID cannot be empty.")
 
-    params = {
+    query_params = {
         "tripid": tripid,
         "time": time,
         "stopid": stopid,
         "mapdata": mapdata
     }
 
+    return query_vvo_api(defaulturl, default_headers, query_params)
 
-
-def vvo_api_query_trip(origin: str, destination: str, shorttermchanges: bool = False, time: str = "", isArrivalTime: bool = False) -> None:
+def vvo_api_query_trip(origin: str, destination: str, shorttermchanges: bool = False, time: str = "", isArrivalTime: bool = False):
     """
     Query how to get from station "Hauptbahnhof" (stopid 33000028) to station "Bahnhof Neustadt" (stopid 33000016).
     Arguments:
@@ -144,12 +158,12 @@ def vvo_api_query_trip(origin: str, destination: str, shorttermchanges: bool = F
         isArrivalTime : Is the time specified above supposed to be interpreted as arrival or departure time?
     """
     defaulturl = "https://webapi.vvo-online.de/tr/trips"
-    headers["X-Requested-With"] = "de.dvb.dvbmobil"
+    default_headers["X-Requested-With"] = "de.dvb.dvbmobil"
     
     if not origin or not destination:
         raise ValueError("Origin or destination cannot be empty.")
 
-    params = {
+    query_params = {
         "origin": origin,
         "destination": destination,
         "shorttermchanges": shorttermchanges,
@@ -157,9 +171,9 @@ def vvo_api_query_trip(origin: str, destination: str, shorttermchanges: bool = F
         "isArrivalTime": isArrivalTime
     }
 
+    return query_vvo_api(defaulturl, default_headers, query_params)
 
-
-def vvo_api_route_changes(shortterm: bool = True) -> None:
+def vvo_api_route_changes(shortterm: bool = True):
     """
     Get information about route changes because of construction work or such.
     Arguments:
@@ -167,11 +181,11 @@ def vvo_api_route_changes(shortterm: bool = True) -> None:
     """
     defaulturl = "https://webapi.vvo-online.de/rc"
 
-    params = {"shortterm": shortterm}
+    query_params = {"shortterm": shortterm}
 
+    return query_vvo_api(defaulturl, default_headers, query_params)
 
-
-def vvo_api_lines(stopid: str) -> None:
+def vvo_api_lines(stopid: str):
     """
     Get informatin about wich lines do service a stop.
     """
@@ -179,27 +193,9 @@ def vvo_api_lines(stopid: str) -> None:
     if not stopid:
         raise ValueError("Stop ID cannot be empty.")
     
-    params = {"stopid": stopid}
+    query_params = {"stopid": stopid}
     
-    try:
-        response = requests.get(defaulturl, params=params, headers=headers, timeout=5, verify=True) 
-        if response.status_code == 200:
-            content = json.loads(response.content.decode('utf-8'))
-            # response {'PointStatus': 'Identified', 'Status': {'Code': 'Ok'}, 'Points': ['33000313|||Räcknitzhöhe|5655709|4622355|0||'], 'ExpirationTime': '/Date(1753115786301+0200)/'}
-            
-            #output = content['Points'][0].split('|')
-            # outputs the first point in the list, e.g. ['33000313', '', 'Räcknitzhöhe', '5655709', '4622355', '0', '']
-            
-            return content
-        else:
-            raise requests.HTTPError('HTTP Status: {}'.format(response.status_code))    
-    except requests.RequestException as e:
-        print(f"Failed to access VVO pointfinder. Request Exception", e)
-        response = None
-    
-    if response is None:
-        return None
-    
+    return query_vvo_api(defaulturl, default_headers, query_params)
 
 
 def vvo_timestamp_to_datetime_class(input: str):
