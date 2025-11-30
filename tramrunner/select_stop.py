980ddedda4f4998d2bcd983e64dfee5d07b6678e
cmdr <cmdr@commander23.net>
2025-11-30 10:16:28 +0100

    deleted more old stuff

--- tramrunner/select_stop.py
+++
@@ -1,3 +0,0 @@
-from vvo_api import * #vvo_api_pointfinder, vvo_api_departure_monitor, vvo_api_trip_details, vvo_api_query_trip, vvo_api_lines, vvo_timestamp_to_datetime_class, get_stop_id_from_pointfinder
-from static_vvo import write_to_json, web_get_json, load_geojson, search_geojson
-from datetime import datetime, timedelta
