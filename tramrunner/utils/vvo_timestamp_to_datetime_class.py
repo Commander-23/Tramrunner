4dff7eac8eef5dfbe5cfb53f74d22174864dca6d
cmdr <cmdr@commander23.net>
2025-12-05 00:28:12 +0100

    model output data

--- tramrunner/utils/vvo_timestamp_to_datetime_class.py
+++ tramrunner/utils/vvo_timestamp_to_datetime_class.py
@@ -1,14 +1,10 @@
 from datetime import datetime, timedelta
 import re
-def vvo_timestamp_to_datetime_class(dvb_time: str):
+def vvo_timestamp_to_datetime_class(dvb_time_noformat: str):
     """
     Pattern
 
-        PATTERN = re.compile(r"/Date\((?P<ms>-?\d+)(?P<tz>[+-]\d{4})?\)/")
-        m = PATTERN.match(dvb_timestamp)
-        ms = int(m.group("ms"))
-        tz = m.group("tz")
-        
+
         sign = 1 if tz_str[0] == "+" else -1
         tz_hours = int(tz[1:3])
         tz_minutes = int(tz[3:5])
@@ -17,14 +13,20 @@ def vvo_timestamp_to_datetime_class(dvb_time: str):
 
 
     """
-
-
-
-    # /Date(1753482300000-0000)/
     PATTERN = re.compile(r"/Date\((?P<ms>-?\d+)(?P<tz>[+-]\d{4})?\)/")
-    matched_str = PATTERN.match(dvb_time)
+    dvb_time = PATTERN.match(dvb_time_noformat)
+    timestamp_ms = int(dvb_time.group("ms"))
+    timezone_str = dvb_time.group("tz")
 
 
+    timezone_sign = 1 if timezone_str[0] == "+" else -1
+    timezone_hours = int(timezone_str[1:3]) 
+    timezone_minutes = int(timezone_str[3:5])
+    timezone_offset = timedelta(timezone_hours, timezone_minutes) * timezone_sign
+    
+    
+    #out = {timestamp_ms, timezone_str, timezone_sign, timezone_hours, timezone_minutes}
+    #return  out
 
 
     human_readable_time = datetime.fromtimestamp(int(dvb_time[6:-7])/1000)
@@ -32,5 +34,7 @@ def vvo_timestamp_to_datetime_class(dvb_time: str):
     tz_offset_hh = int(dvb_time[19:-4])
     tz_offset_mm = int(dvb_time[22:-2])
     timezone_delta = timedelta(hours=tz_offset_hh, minutes=tz_offset_mm)
-    
     return human_readable_time, timezone_delta
+
+if __name__ == "__main__":
+    print(vvo_timestamp_to_datetime_class("/Date(1764662530512+0100)/"))
