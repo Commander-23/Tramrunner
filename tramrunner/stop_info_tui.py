01cb84a3019ae632d34a076c419a85b4b283a3e8
cmdr <cmdr@commander23.net>
2025-12-05 21:09:28 +0100

    works with time differential display, and no mor try exept

--- tramrunner/stop_info_tui.py
+++ tramrunner/stop_info_tui.py
@@ -1,4 +1,5 @@
 import utils, api
+from datetime import datetime
 
 def stop_info_tui(userinput:str=None):
     if userinput is not None:
@@ -7,30 +8,49 @@ def stop_info_tui(userinput:str=None):
         print("Please provide a stop name or ID.")
         return None
     
-    try:
-        departure_monitor_response = api.vvo_departure_monitor(stopid, limit=15)
-        station_name = departure_monitor_response['Name']
-        station_city = departure_monitor_response['Place']
-        expiration_time = utils.vvo_timestamp_to_datetime_class(departure_monitor_response['ExpirationTime'])[0]
-        expiration_time_human = expiration_time.strftime("%H:%M:%S")
+    #try:
+    departure_monitor_response = api.vvo_departure_monitor(stopid, limit=15)
+    station_name = departure_monitor_response['Name']
+    station_city = departure_monitor_response['Place']
+    expiration_time = utils.vvo_timestamp_to_datetime_class(departure_monitor_response['ExpirationTime'])
+    expiration_time_human = expiration_time.strftime("%H:%M:%S")
 
-        output_lines = [
-            f"\nAbfahrten für '{station_name}':\nUngültig in {expiration_time_human}",
-            f"{'Linie':<8}{'Richtung':<30}{'Ankunft':<10}",
-            "-" * 48
-        ]
-        for departure in departure_monitor_response['Departures']:
-            line_number = departure.get('LineName')
-            line_direction = departure.get('Direction')
-            arrival_real_time = utils.vvo_timestamp_to_datetime_class(departure.get('RealTime'))[0].strftime("%H:%M:%S")
-            arrival_scheduled_time = utils.vvo_timestamp_to_datetime_class(departure.get('ScheduledTime'))[0].strftime("%H:%M:%S")
+    output_lines = [
+        f"\nAbfahrten für '{station_name}':\nUngültig in {expiration_time_human}",
+        f"{'Linie':<8}{'Richtung':<30}{'Ankunft':<10}",
+        "-" * 48
+    ]
+    for departure in departure_monitor_response['Departures']:
+        line_number = departure.get('LineName')
+        line_direction = departure.get('Direction')
 
-            output_lines.append(f"{line_number:<8}{line_direction:<30}{arrival_real_time:<10}")
-        return "\n".join(output_lines)
-    except Exception as e:
-        print("\aAnError Occured")
-        print(e)
-    return None
+        arrival_scheduled_time = utils.vvo_timestamp_to_datetime_class(departure.get('ScheduledTime')).astimezone().strftime("%H:%M:%S")
+        
+        arrival_display = arrival_scheduled_time
+
+        if departure.get('RealTime'):
+            arrival_real_time = utils.vvo_timestamp_to_datetime_class(departure.get('RealTime'))
+            time_now = datetime.now(arrival_real_time.tzinfo) if arrival_real_time.tzinfo else datetime.now()
+            arrival_real_diff = arrival_real_time - time_now
+
+            arrival_seconds = int(arrival_real_diff.total_seconds())
+
+            if arrival_seconds <= 0:
+                arrival_in = f"{arrival_seconds} seconds"
+            elif arrival_seconds < 60:
+                arrival_in = f"{arrival_seconds} seconds"
+            else:
+                arrival_minutes = arrival_seconds // 60
+                arrival_in = f"in {arrival_minutes} min"
+
+            arrival_display = f"{arrival_real_time.astimezone().strftime('%H:%M')} ({arrival_in})"
+
+        output_lines.append(f"{line_number:<8}{line_direction:<30}{arrival_display}")
+    return "\n".join(output_lines)
+    #except Exception as e:
+    #    print("\aAn Error Occured")
+    #    print(e)
+    #return None
 
 
 if __name__ == "__main__":
