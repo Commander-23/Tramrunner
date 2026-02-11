ab0151a6334fa1c532c796ef45a3deff6991c778
cmdr <cmdr@commander23.net>
2026-02-11 02:42:22 +0100

    css & better fill-tram-info

--- tramrunner/tramrunner_textual.py
+++ tramrunner/tramrunner_textual.py
@@ -21,29 +21,57 @@ class StopInfoHeader(HorizontalGroup):
 
 class StopInfoSingleTram(HorizontalGroup):
     def compose(self) -> ComposeResult:
-        yield Digits("", id="departure_number_name")
+        yield Digits("", id="departure_line_number")
+        with VerticalGroup():
+            yield Label("label", id="label1", classes="tram_label")
+            yield Label("label", id="label2", classes="tram_label")
+            yield Label("label", id="label3", classes="tram_label")
         with VerticalGroup():
             yield Static("", id="departure_time", classes="tram_label")
             yield Static("", id="departure_real_time", classes="tram_label")
+            yield Label("label", id="label11", classes="tram_label")
         with VerticalGroup():
             yield Static("", id="mot_type", classes="tram_label")
             yield Static("", id="direction_name", classes="tram_label")
-        with VerticalGroup():
-            yield Label("label", classes="tram_label")
-            yield Label("label", classes="tram_label")
 
     
     def fill_tram_info(self, departure_data):
-        if 'RealTime' in departure_data:
-            real_time = utils.vvo_timestamp_to_datetime_class(departure_data['RealTime']).strftime("%H:%M")
+        
+        # Time Stuff
+        depa_realtime = departure_data.get('RealTime')
+        depa_time = departure_data.get('ScheduledTime')
+        if depa_realtime: real_time = utils.vvo_time_conv(depa_realtime).strftime("%H:%M")
         else: real_time = f"--:--"
-        depa_time = utils.vvo_timestamp_to_datetime_class(departure_data['ScheduledTime']).strftime("%H:%M")
+        line_time = utils.vvo_time_conv(depa_time).strftime("%H:%M")
+        depa_state = departure_data.get('State')
+        # ID Stuff
+        depa_id = departure_data.get('Id')    
+        depa_dlid = departure_data.get('DlId')    
+        depa_diva = departure_data.get('Diva')
 
-        self.query_one("#departure_number_name", Digits).update(departure_data['LineName'])
-        self.query_one("#direction_name", Static).update(departure_data['Direction'])
-        self.query_one("#mot_type", Static).update(departure_data['Mot'])
+        # schedule stuff
+        depa_routechanges = departure_data.get('RouteChanges')
+        depa_ocup = departure_data.get('Occupancy')            
+        depa_cancel_res = departure_data.get('CacelReasons')
+
+        # line info stuff    
+        depa_mot = departure_data.get('Mot')
+        line_name = departure_data.get('LineName')
+        line_direction = departure_data.get('Direction')
+        depa_platform = departure_data.get('Platform')
+        
+        # seperating line_name into digits and charachters
+        line_digits = "".join([char for char in line_name if char.isdigit()])
+        line_char = "".join([char for char in line_name if char.isalpha()])
+
+        self.query_one("#label1", Label).update(f"{"".join(line_digits)}")
+        self.query_one("#label2", Label).update(f"{"".join(line_char)}")
+
+        self.query_one("#departure_line_number", Digits).update(line_digits)
+        self.query_one("#direction_name", Static).update(line_direction)
+        self.query_one("#mot_type", Static).update(depa_mot)
         self.query_one("#departure_time", Static).update(f"{real_time} live")
-        self.query_one("#departure_real_time", Static).update(f"{depa_time}")
+        self.query_one("#departure_real_time", Static).update(f"{line_time}")
         
 class SingleTram(HorizontalGroup):
     def compose(self) -> ComposeResult:
@@ -93,16 +121,17 @@ class Tramrunner(App):
 
         #
         stopid = utils.get_stop_id_from_pointfinder(stop_info_text_input.value)
-        stop_info_data = api.vvo_departure_monitor(stopid)
+        stop_info_data = api.vvo_departure_monitor(stopid, limit=30)
 
         stop_name.content = stop_info_data['Name']
         stop_place.content = stop_info_data['Place']
         stop_expiry1.content = stopid
-        stop_expiry2.content = utils.vvo_timestamp_to_datetime_class(stop_info_data['ExpirationTime']).strftime("%H:%M:%S")
+        stop_expiry2.content = utils.vvo_time_conv(stop_info_data['ExpirationTime']).strftime("%H:%M:%S")
         logger.write(stop_info_data)
 
         for departure in stop_info_data['Departures']:
-            new_tram = StopInfoSingleTram()
+
+            new_tram = StopInfoSingleTram(classes="single_departure, alternate")
             scroller.mount(new_tram)
             scroller.scroll_visible()
             new_tram.fill_tram_info(departure)
