9bcafcc5fdc173f7db7cb7cbed306d6eb009cc87
cmdr <cmdr@commander23.net>
2026-04-08 17:50:24 +0200

    bissl testen der pointfinder funktion

--- tramrunner/api/vvo_pointfinder.py
+++ tramrunner/api/vvo_pointfinder.py
@@ -1,9 +1,8 @@
-from api import query_vvo_api
+from .query_vvo_api import query_vvo_api
 def vvo_pointfinder(query: str, limit: int = 0, stopsOnly: bool = False, regionalOnly: bool = False, stopShortcuts: bool = False):
     """
     Find stops based certain parameters.
     """
-   
     defaulturl = "https://webapi.vvo-online.de/tr/pointfinder"
 
     if not query:
@@ -19,4 +18,18 @@ def vvo_pointfinder(query: str, limit: int = 0, stopsOnly: bool = False, regiona
     }
     # {'query': 'Räcknitzhöhe', 'limit': 10, 'stopsOnly': True, 'regionalOnly': False, 'stopShortcuts': False}
     return query_vvo_api(defaulturl, None, query_params)
- 
+
+if __name__ == "__main__":
+    query_params = {
+        "query": "wiener",
+        "limit": 5,
+        "stopsOnly": True,
+        "regionalOnly": True,
+        "stopShortcuts": False
+    }
+    data = vvo_pointfinder(**query_params)
+    print(f"search Status:  {data['PointStatus']}")
+    print(f"request status: {data['Status']}")
+    print(f"expiry-time:    {data['ExpirationTime']}")
+    for point in data['Points']:
+        print(point)
